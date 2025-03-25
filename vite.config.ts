ac16743737326673e4f9660b5c9c0ff59bdc1700
Piotr Gąsiorek <35469939+PiotrGasiorek@users.noreply.github.com>
2025-03-25 23:44:18 +0100

    Replace default styles with theme

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "sass:map";
+          @use "sass:meta";
+          @use "${path.resolve(
+            __dirname,
+            "./src/styles/variables/_palette.scss"
+          )}" as palette;
+          @use "${path.resolve(
+            __dirname,
+            "./src/styles/variables/_spacing.scss"
+          )}" as spacing;
+          @use "${path.resolve(
+            __dirname,
+            "./src/styles/variables/_surface.scss"
+          )}" as surface;
+          @use "${path.resolve(
+            __dirname,
+            "./src/styles/_reset.scss"
+          )}" as reset;
+          @use "${path.resolve(
+            __dirname,
+            "./src/styles/_global.scss"
+          )}" as global;
+          @mixin text-size($size) {
+            font-size: var(--font-size-#{$size});
+          }
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
