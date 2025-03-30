43875dc0ee765053d5fa0dd6a956aba2b5f39c8d
Piotr Gąsiorek <35469939+PiotrGasiorek@users.noreply.github.com>
2025-03-30 10:12:00 +0200

    Setup Dashboard Page UI

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,5 +1,29 @@
+import { Typography } from "../../components/Typography";
+import { GamesTable } from "../../features/GamesTable";
+import { useGameHistory } from "../../hooks/useGameRecords";
 import "./DashboardPage.scss";
 
 export const DashboardPage = () => {
-  return <>DashboardPage</>;
+  const { pastGames } = useGameHistory();
+
+  return (
+    <div className="dashboardPage">
+      <Typography className="dashboardPage__title" component="h1" variant="h3">
+        Statistics
+      </Typography>
+      {pastGames.length ? (
+        <div className="dashboardPage__tableWrapper">
+          <GamesTable />
+        </div>
+      ) : (
+        <Typography
+          className="dashboardPage__fallback"
+          component="h2"
+          variant="h5"
+        >
+          Ooops! You haven't played any games in the past
+        </Typography>
+      )}
+    </div>
+  );
 };
